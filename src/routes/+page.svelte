0abe6a5d8736d0e33bfe98817e40e120bcc289d3
phandung2101 <dungpv.dev@gmail.com>
2025-04-01 21:28:34 +0700

    add daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import "../style.css";
+</script>
+<button class="btn w-64 rounded-full">Button</button>
